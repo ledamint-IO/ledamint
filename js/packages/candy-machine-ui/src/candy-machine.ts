9f03487853f56130b9bddec462aaa77d7aae786d
owlman2222 <54411950+j0nnyboi@users.noreply.github.com>
2022-08-05 13:34:24 +0100

    update

--- js/packages/candy-machine-ui/src/candy-machine.ts
+++ js/packages/candy-machine-ui/src/candy-machine.ts
@@ -21,7 +21,7 @@ export const CANDY_MACHINE_PROGRAM = new anchor.web3.PublicKey(
 );
 
 const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
-  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+  'WbMTNyvtk8vSMu2AmXV7mKuYrADRNw9GSkNtWKsZ7qe',
 );
 
 interface CandyMachineState {
